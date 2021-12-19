c62fe5a25fce828f43be8379fdd8e136bf127c03
Felix Nüsse <felix.nuesse@t-online.de>
2021-12-19 11:37:53 +0100

    add fastlane screenshot capability
    Signed-off-by: Felix Nüsse <felix.nuesse@t-online.de>

--- app/src/androidTest/java/de/felixnuesse/timedsilence/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/felixnuesse/timedsilence/ExampleInstrumentedTest.kt
@@ -1,24 +1,44 @@
 package de.felixnuesse.timedsilence
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.ClassRule
+import org.junit.runners.JUnit4
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.locale.LocaleTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(JUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    public var rule: ActivityScenarioRule<*> = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("de.felixnuesse.timedsilence", appContext.packageName)
+    fun testTakeScreenshot() {
+        Screengrab.screenshot("before_button_click")
+
+        // Your custom onView...
+        onView(withId(R.id.fab)).perform(click())
+        Screengrab.screenshot("after_button_click")
+    }
+
+    companion object {
+        @ClassRule @JvmField
+        val localeTestRule: LocaleTestRule = LocaleTestRule()
     }
 }
