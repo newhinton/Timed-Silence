1ce5e5fc96c6146200111638729211fc72a9d3bb
Felix Nüsse <felix.nuesse@t-online.de>
2023-12-13 18:08:57 +0100

    style calendar row better for higher contrast

--- app/src/main/java/de/felixnuesse/timedsilence/ui/CalendarListAdapter.kt
+++ app/src/main/java/de/felixnuesse/timedsilence/ui/CalendarListAdapter.kt
@@ -2,21 +2,24 @@ package de.felixnuesse.timedsilence.ui;
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Resources.Theme
+import android.graphics.Color
 import android.graphics.Typeface
-import androidx.recyclerview.widget.RecyclerView
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.RecyclerView
+import de.felixnuesse.timedsilence.R
+import de.felixnuesse.timedsilence.databinding.AdapterCalendarListBinding
 import de.felixnuesse.timedsilence.handler.volume.VolumeState.Companion.TIME_SETTING_LOUD
 import de.felixnuesse.timedsilence.handler.volume.VolumeState.Companion.TIME_SETTING_SILENT
 import de.felixnuesse.timedsilence.handler.volume.VolumeState.Companion.TIME_SETTING_VIBRATE
-import de.felixnuesse.timedsilence.R
-import de.felixnuesse.timedsilence.databinding.AdapterCalendarListBinding
 import de.felixnuesse.timedsilence.model.data.CalendarObject
 import de.felixnuesse.timedsilence.model.database.DatabaseHandler
 import de.felixnuesse.timedsilence.volumestate.calendar.DeviceCalendar
-import kotlin.collections.ArrayList
 
 
 /**
@@ -70,17 +73,29 @@ class CalendarListAdapter(private var myDataset: ArrayList<CalendarObject>, priv
                 // - get element from your dataset at this position
                 // - replace the contents of the view with that element
                 val calObject= myDataset[position]
+                val context = holder.calendarView.root.context
 
                 holder.calendarView.textViewCalendarRowTitle.text = calObject.name
                 holder.calendarView.deleteCalendarElement.setOnClickListener {
-                        DatabaseHandler(holder.calendarView.root.context).deleteCalendarEntry(calObject.id)
+                        DatabaseHandler(context).deleteCalendarEntry(calObject.id)
                         removeAt(position)
                 }
 
+                val color = calHandler.getCalendarColor(calObject.name)
+                holder.calendarView.cardView.setCardBackgroundColor(color)
+                holder.calendarView.deleteCalendarElement.setColorFilter(color)
+
                 applyTextfieldStyle(holder.calendarView.textViewCalendarRowTitle)
 
-                holder.calendarView.cardView.setCardBackgroundColor(calHandler.getCalendarColor(calObject.name))
-                holder.calendarView.deleteCalendarElement.setColorFilter(calHandler.getCalendarColor(calObject.name))
+                if(Color.luminance(color) > 0.55) {
+
+                        val typedValue = TypedValue()
+                        val theme: Theme = context.theme
+                        theme.resolveAttribute(R.attr.colorOnPrimary, typedValue, true)
+                        holder.calendarView.textViewCalendarRowTitle.setTextColor(typedValue.data)
+                        holder.calendarView.volumeState.imageTintList = ColorStateList.valueOf(typedValue.data)
+                }
+
 
                 var imageID=R.drawable.icon_volume_up
                 when (calObject.volume) {
@@ -88,7 +103,7 @@ class CalendarListAdapter(private var myDataset: ArrayList<CalendarObject>, priv
                         TIME_SETTING_VIBRATE -> imageID=R.drawable.icon_vibration
                         TIME_SETTING_SILENT -> imageID=R.drawable.icon_volume_off
                 }
-                holder.calendarView.volumeState.setImageDrawable(AppCompatResources.getDrawable(holder.calendarView.root.context, imageID))
+                holder.calendarView.volumeState.setImageDrawable(AppCompatResources.getDrawable(context, imageID))
         }
 
         private fun applyTextfieldStyle(view: TextView){
