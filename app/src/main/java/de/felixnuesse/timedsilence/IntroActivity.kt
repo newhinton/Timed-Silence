352bdb12603a6c7c0d769e8431281faf28115d07
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-17 09:30:00 +0200

    Update IntroActivity with SlideLeaveInterface

--- app/src/main/java/de/felixnuesse/timedsilence/IntroActivity.kt
+++ app/src/main/java/de/felixnuesse/timedsilence/IntroActivity.kt
@@ -1,6 +1,7 @@
 package de.felixnuesse.timedsilence
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -11,18 +12,24 @@ import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment
+import de.felixnuesse.disky.ui.appintro.IdentifiableAppIntroFragment
+import de.felixnuesse.disky.ui.appintro.SlideLeaveInterface
 import de.felixnuesse.timedsilence.util.PermissionManager
 import de.felixnuesse.timedsilence.util.PrepareDefaultsUtil
 
-class IntroActivity : AppIntro() {
+class IntroActivity : AppIntro(), SlideLeaveInterface {
 
 
     companion object {
         const val INTRO_PREFERENCES = "IntroPreferences"
+
+        private const val SLIDE_ID_WELCOME = "SLIDE_ID_WELCOME"
+        private const val SLIDE_ID_CALENDAR = "SLIDE_ID_CALENDAR"
+        private const val SLIDE_ID_NOTIFICATIONS = "SLIDE_ID_NOTIFICATIONS"
+        private const val SLIDE_ID_DND = "SLIDE_ID_DND"
     }
 
     private var permissionManager = PermissionManager(this)
-    private var isDoNotDisturbSlide = false
 
 
     override fun onResume() {
@@ -40,11 +47,6 @@ class IntroActivity : AppIntro() {
         // Make sure you don't call setContentView!
 
         setImmersiveMode()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-        }
-
         showStatusBar(true)
         isWizardMode = true
         isColorTransitionsEnabled = true
@@ -56,19 +58,23 @@ class IntroActivity : AppIntro() {
         // Call addSlide passing your Fragments.
         // You can use AppIntroFragment to use a pre-built fragment
         addSlide(
-            AppIntroFragment.createInstance(
+            IdentifiableAppIntroFragment.createInstance(
                 title = getString(R.string.intro_slide_welcome_title),
                 description = getString(R.string.intro_slide_welcome_description),
                 imageDrawable = R.drawable.undraw_reading_time,
-                backgroundColorRes = R.color.intro_color1
+                backgroundColorRes = R.color.intro_color1,
+                id = SLIDE_ID_WELCOME,
+                callback = this
             ))
 
         addSlide(
-            AppIntroFragment.createInstance(
+            IdentifiableAppIntroFragment.createInstance(
                 title = getString(R.string.intro_slide_calendar_title),
                 description = getString(R.string.intro_slide_calendar_description),
                 imageDrawable = R.drawable.undraw_schedule,
-                backgroundColorRes = R.color.intro_color2
+                backgroundColorRes = R.color.intro_color2,
+                id = SLIDE_ID_CALENDAR,
+                callback = this
             ))
         // Regarding Slide Numbers: They index with 0. But since we want to show them AFTER the slide,
         // We have to offset them by one, and assume 1-indexing.
@@ -80,11 +86,13 @@ class IntroActivity : AppIntro() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             addSlide(
-                AppIntroFragment.createInstance(
+                IdentifiableAppIntroFragment.createInstance(
                     title = getString(R.string.intro_slide_notification_title),
                     description = getString(R.string.intro_slide_notification_description),
                     imageDrawable = R.drawable.undraw_notify,
-                    backgroundColorRes = R.color.intro_color1
+                    backgroundColorRes = R.color.intro_color1,
+                    id = SLIDE_ID_NOTIFICATIONS,
+                    callback = this
                 ))
             askForPermissions(
                 permissions = arrayOf(Manifest.permission.POST_NOTIFICATIONS),
@@ -93,13 +101,14 @@ class IntroActivity : AppIntro() {
         }
 
         addSlide(
-            AppIntroFragment.createInstance(
+            IdentifiableAppIntroFragment.createInstance(
                 title = getString(R.string.intro_slide_donotdisturb_title),
                 description = getString(R.string.intro_slide_donotdisturb_description),
                 imageDrawable = R.drawable.undraw_camping,
-                backgroundColorRes = R.color.intro_color2
+                backgroundColorRes = R.color.intro_color2,
+                id = SLIDE_ID_DND,
+                callback = this
             ))
-
 
         addSlide(
             AppIntroFragment.createInstance(
@@ -110,18 +119,6 @@ class IntroActivity : AppIntro() {
             ))
     }
 
-
-    override fun onPageSelected(position: Int) {
-        //check if we *were* on the dnd slide on the last slide, and ask for dnd. Then update slide.
-        if(isDoNotDisturbSlide) {
-            if(!permissionManager.grantedDoNotDisturb()) {
-                permissionManager.requestDoNotDisturb()
-            }
-        }
-
-        val pageID = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { 3 } else { 2 }
-        isDoNotDisturbSlide = position == pageID
-    }
 
 
     override fun onSkipPressed(currentFragment: Fragment?) {
@@ -148,4 +145,19 @@ class IntroActivity : AppIntro() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
+
+    override fun allowSlideLeave(id: String): Boolean {
+        return when(id) {
+            SLIDE_ID_DND -> permissionManager.grantedDoNotDisturb()
+            else -> true
+        }
+    }
+
+    override fun onSlideLeavePrevented(id: String) {
+        when(id) {
+            SLIDE_ID_DND -> permissionManager.requestDoNotDisturb()
+            else -> {}
+        }
+    }
+
 }
